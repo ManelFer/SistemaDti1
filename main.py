149001f550cb3bc08dd4d08e8973f9f095d41760
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2024-11-22 10:44:50 -0300

    alteração
    Feita a função para ver se conecta

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ class Login(QMainWindow, Ui_Login):
         appIcon = QIcon(u"src_img/icons/")
         self.setWindowIcon(appIcon)
 
+    def open_system(self):
+        if self.line_pass.text() == '123':
+            self.w = Ui_Main()
+            self.w.show()
+            self.close()
+        else:
+            print('Senha inválida')
+
+class Main(QMainWindow, Ui_Main):
+    def __init__(self):
+        super(Main,self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle("DTI - Sistema de controle")
+
+        #****************PAGINAS DO SISTEMA****************
+        self.btn_home.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_1_home))
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Login()
